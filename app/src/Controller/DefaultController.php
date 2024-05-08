8976b5e2b42c3d4988109c5d63011c7e09a9fb1b
tommypria <tommypriambodo@gmail.com>
2024-05-08 11:50:34 +0700

    setup layout

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Pimcore\Bundle\AdminBundle\Controller\Admin\LoginController;
 use Pimcore\Controller\FrontendController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends FrontendController
 {
@@ -24,5 +25,11 @@ class DefaultController extends FrontendController
     public function loginAction(): Response
     {
         return $this->forward(LoginController::class.'::loginCheckAction');
+    }
+
+    #[Route('/test')]
+    public function testAction(): Response
+    {
+        return $this->render('test.html.twig', ['data' => 'value1']);
     }
 }
